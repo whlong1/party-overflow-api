3c84c43d41bd130f0b752a7b2872143c04c83436
whlong1 <whunterlong@gmail.com>
2022-06-14 22:14:56 -0400

    add comment schema

--- models/post.js
+++ models/post.js
@@ -2,6 +2,25 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const commentSchema = new Schema(
+  {
+    comment_text: {
+      type: String,
+      required: true
+    },
+    codeblock: {
+      type: String,
+      required: false
+    },
+    is_solution: {
+      type: Boolean,
+      default: false
+    },
+    commenter: { type: mongoose.Schema.Types.ObjectId, ref: 'Profile' }
+  },
+  { timestamps: true }
+)
+
 const postSchema = new Schema({
   question: {
     type: String,
@@ -16,6 +35,7 @@ const postSchema = new Schema({
     default: false
   },
   added_by: { type: mongoose.Schema.Types.ObjectId, ref: 'Profile' },
+  comments: [commentSchema]
 }, { timestamps: true })
 
 
