31889f20241c32f9f3857330bf6e7c4768b64087
whlong1 <whunterlong@gmail.com>
2022-06-18 15:43:01 -0400

    build out posts show controller

--- controllers/posts.js
+++ controllers/posts.js
@@ -25,11 +25,16 @@ const index = async (req, res) => {
   }
 }
 
-
 const show = async (req, res) => {
-
+  try {
+    const post = await Post.findById(req.params.id)
+      .populate('author')
+      .populate('comments.author')
+    return res.status(200).json(post)
+  } catch (err) {
+    return res.status(500).json(err)
+  }
 }
-
 
 export {
   index,
