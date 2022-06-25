610c0b05b52d4e3687cf84b3b55fe77aacc1de04
whlong1 <whunterlong@gmail.com>
2022-06-24 23:36:08 -0400

    add sort and limit methods to profile index controller

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -3,6 +3,8 @@ import { Profile } from "../models/profile.js";
 const index = async (req, res) => {
   try {
     const profiles = await Profile.find({})
+      .sort({ solution_count: -1 })
+      .limit(10)
     res.status(200).json(profiles)
   } catch (err) {
     res.status(500).json(err)
