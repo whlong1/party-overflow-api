e7349dcee216c3e523d686ba833e32df5be93bc4
whlong1 <whunterlong@gmail.com>
2022-06-26 13:50:58 -0400

    refactor post's show controller to enable comment pagination

--- controllers/posts.js
+++ controllers/posts.js
@@ -32,7 +32,9 @@ const index = async (req, res) => {
 
 const show = async (req, res) => {
   try {
-    const post = await Post.findById(req.params.id)
+    const count = 3
+    const page = req.query.page ? req.query.page : 0
+    const post = await Post.findById(req.params.id, { comments: { $slice: [page, count] } })
       .populate('author', 'name avatar')
       .populate('comments.author', { select: 'name avatar' })
     res.status(200).json(post)
