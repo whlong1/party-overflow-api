4ad9fd46d795e99256876e880084877996eca942
whlong1 <whunterlong@gmail.com>
2022-07-02 15:14:08 -0400

    refactor signup controller to use async await

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,50 +2,47 @@ import { User } from '../models/user.js'
 import { Profile } from '../models/profile.js'
 import jwt from 'jsonwebtoken'
 
-function signup(req, res) {
-  Profile.findOne({ email: req.body.email })
-  .then(profile => {
+const signup = async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ email: req.body.email })
     if (profile) {
       throw new Error('Account already exists')
-    } else if (!process.env.SECRET){
+    } else if (!process.env.SECRET) {
       throw new Error('no SECRET in .env file')
     } else {
-      Profile.create(req.body)
-      .then(newProfile => {
-        req.body.profile = newProfile._id
-        User.create(req.body)
-        .then(user => {
-          const token = createJWT(user)
-          res.status(200).json({ token })
-        })
-        .catch(err => {
-          Profile.findByIdAndDelete(newProfile._id)
-          res.status(500).json({err: err.errmsg})
-        })
-      })
+      var newProfile = await Profile.create(req.body)
+      req.body.profile = newProfile._id
+      const user = await User.create(req.body)
+      const token = createJWT(user)
+      res.status(200).json({ token })
     }
-  })
-  .catch(err => {
-    res.status(500).json({err: err.message})
-  })
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      await Profile.findByIdAndDelete(newProfile._id)
+      res.status(500).json({ err: err.message })
+    } else {
+      res.status(500).json({ err: err.message })
+    }
+  }
 }
+
 
 function login(req, res) {
   User.findOne({ email: req.body.email })
-  .then(user => {
-    if (!user) return res.status(401).json({ err: 'User not found' })
-    user.comparePassword(req.body.pw, (err, isMatch) => {
-      if (isMatch) {
-        const token = createJWT(user)
-        res.json({ token })
-      } else {
-        res.status(401).json({ err: 'Incorrect password'})
-      }
+    .then(user => {
+      if (!user) return res.status(401).json({ err: 'User not found' })
+      user.comparePassword(req.body.pw, (err, isMatch) => {
+        if (isMatch) {
+          const token = createJWT(user)
+          res.json({ token })
+        } else {
+          res.status(401).json({ err: 'Incorrect password' })
+        }
+      })
     })
-  })
-  .catch(err => {
-    res.status(500).json(err)
-  })
+    .catch(err => {
+      res.status(500).json(err)
+    })
 }
 
 function createJWT(user) {
@@ -56,7 +53,7 @@ function createJWT(user) {
   )
 }
 
-export { 
+export {
   signup,
   login
 }
