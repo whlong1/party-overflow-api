1d49d1618608eeb5a6584fcd6083689c5e0cda50
whlong1 <whunterlong@gmail.com>
2022-07-02 14:19:35 -0400

    add field specs in profiles index controller

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -2,7 +2,7 @@ import { Profile } from "../models/profile.js";
 
 const index = async (req, res) => {
   try {
-    const profiles = await Profile.find({})
+    const profiles = await Profile.find({}, 'name avatar solution_count')
       .sort({ solution_count: -1 })
       .limit(10)
     res.status(200).json(profiles)
@@ -24,10 +24,8 @@ const follow = async (req, res) => {
   try {
     const follower = Profile.findById(req.user.profile)
     const followee = Profile.findById(req.params.id)
-
     follower.following.push(followee._id)
     followee.followers.push(follower._id)
-
     await Promise.all([follower.save(), followee.save()])
     res.status(200).json({ msg: 'Success' })
   } catch (err) {
