5c68c1872fc63178913465e315ff141b55497eae
whlong1 <whunterlong@gmail.com>
2022-07-04 21:15:49 -0400

    move postSchema methods to methods module

---
+++ models/post/methods.js
@@ -0,0 +1,54 @@
+import mongoose from 'mongoose'
+
+function findByIdAndSortComments(id, page, limit) {
+  return this.aggregate([
+    // Find post by _id:
+    { $match: { _id: mongoose.Types.ObjectId(id) } },
+    // Fill author field with associated profile
+    {
+      $lookup: {
+        from: 'profiles',
+        localField: 'author',
+        foreignField: '_id',
+        as: 'author'
+      },
+    },
+    // Convert author field from array to single object:
+    { $unwind: '$author' },
+    // Find profiles associated with each comment:
+    { $lookup: { from: "profiles", localField: "comments.author", foreignField: "_id", as: "authors" } },
+    // Add _id, name, and avatar to author property of comment:
+    {
+      $addFields: {
+        "comments.author._id": {
+          $cond: [{ $ne: ["$author", []] }, { $arrayElemAt: ["$authors._id", 0] }, "$comments.author"]
+        },
+        "comments.author.name": {
+          $cond: [{ $ne: ["$author", []] }, { $arrayElemAt: ["$authors.name", 0] }, "$comments.author"]
+        },
+        "comments.author.avatar": {
+          $cond: [{ $ne: ["$author", []] }, { $arrayElemAt: ["$authors.avatar", 0] }, "$comments.author"]
+        }
+      }
+    },
+    // Clean up returned fields:
+    {
+      $project: {
+        _id: 1,
+        text: 1,
+        codeblock: 1,
+        resolved: 1,
+        language: 1,
+        views: 1,
+        author: { _id: 1, name: 1, avatar: 1 },
+        comments: { $slice: ["$comments", page, limit] }
+      }
+    },
+    // Sort comments by rating:
+    { $sort: { "comments.rating": 1 } },
+  ])
+}
+
+export {
+  findByIdAndSortComments
+}
---
+++ models/post/post.js
@@ -0,0 +1,62 @@
+import mongoose from 'mongoose'
+import * as postSchemaMethods from './methods.js'
+
+const Schema = mongoose.Schema
+
+const commentSchema = new Schema(
+  {
+    text: {
+      type: String,
+      required: true
+    },
+    codeblock: {
+      type: String,
+      required: false
+    },
+    solution: {
+      type: Boolean,
+      default: false
+    },
+    rating: {
+      type: Number,
+      default: 0
+    },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'Profile' }
+  },
+  { timestamps: true }
+)
+
+const postSchema = new Schema({
+  text: {
+    type: String,
+    required: true
+  },
+  codeblock: {
+    type: String,
+    required: false
+  },
+  resolved: {
+    type: Boolean,
+    default: false
+  },
+  language: {
+    type: String,
+    required: true,
+    default: 'Javascript',
+    enum: ['HTML', 'CSS', 'Javascript', 'Python']
+  },
+  views: {
+    type: Number,
+    default: 0
+  },
+  comments: [commentSchema],
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'Profile' },
+  viewers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Profile' }]
+}, { timestamps: true })
+
+postSchema.statics = postSchemaMethods
+const Post = mongoose.model('Post', postSchema)
+
+export {
+  Post
+}
