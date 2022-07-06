df3a481bc50fec2698302207d83df35961277a52
whlong1 <whunterlong@gmail.com>
2022-07-05 20:26:33 -0400

    refactor findByIdAndSortComments to join comment and author tables correctly

--- models/post/methods.js
+++ models/post/methods.js
@@ -8,30 +8,45 @@ function findByIdAndSortComments(id, page, limit) {
     { $lookup: { from: 'profiles', localField: 'author', foreignField: '_id', as: 'author' } },
     // Convert author field from array to single object:
     { $unwind: '$author' },
+    // Deconstruct comment array:
+    { $unwind: { path: "$comments", preserveNullAndEmptyArrays: true } },
     // Find profiles associated with each comment:
-    { $lookup: { from: "profiles", localField: "comments.author", foreignField: "_id", as: "authors" } },
-
-    // Add _id, name, and avatar to author property of comment:
     {
-      $addFields: {
-        "comments.author._id": {
-          $cond: [{ $ne: ["$author", []] }, { $arrayElemAt: ["$authors._id", 0] }, "$comments.author"]
-        },
-        "comments.author.name": {
-          $cond: [{ $ne: ["$author", []] }, { $arrayElemAt: ["$authors.name", 0] }, "$comments.author"]
-        },
-        "comments.author.avatar": {
-          $cond: [{ $ne: ["$author", []] }, { $arrayElemAt: ["$authors.avatar", 0] }, "$comments.author"]
-        }
+      $lookup: {
+        from: "profiles",
+        let: { authorId: "$comments.author" },
+        pipeline: [{ $match: { $expr: { $eq: ['$_id', '$$authorId'] } } }, { $project: { _id: 1, name: 1, avatar: 1 } }],
+        as: 'comments.author',
       }
     },
-
+    // Group new object:
+    {
+      $group: {
+        _id: "$_id",
+        text: { $first: "$text" },
+        codeblock: { $first: "$codeblock" },
+        resolved: { $first: "$resolved" },
+        language: { $first: "$language" },
+        views: { $first: "$views" },
+        author: { $first: "$author" },
+        solution: { $first: "$solution" },
+        comments: {
+          $push: {
+            _id: "$comments._id",
+            text: "$comments.text",
+            codeblock: "$comments.codeblock",
+            solution: "$comments.solution",
+            rating: "$comments.rating",
+            author: { $first: "$comments.author" },
+          },
+        },
+      }
+    },
     // Find existing solution:
     { $addFields: { solution: { $filter: { input: "$comments", as: "comment", cond: { $eq: ["$$comment.solution", true] } } } } },
-
     // Sort comments by rating:
-    { $unwind: { path: "$comments", preserveNullAndEmptyArrays: true } }, { $sort: { "comments.rating": -1 } },
-
+    { $unwind: { path: "$comments", preserveNullAndEmptyArrays: true } },
+    { $sort: { "comments.rating": -1 } },
     // Group new object:
     {
       $group: {
@@ -46,10 +61,8 @@ function findByIdAndSortComments(id, page, limit) {
         comments: { $push: "$comments" },
       }
     },
-
     // Remove existing solution from comments array:
     { $addFields: { comments: { $filter: { input: "$comments", as: "comment", cond: { $ne: ["$$comment.solution", true] } } } } },
-
     // Clean up returned fields and apply comment pagination:
     {
       $project: {
