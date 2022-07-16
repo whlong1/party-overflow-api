23c0a31ab9e3c4e01755c993c112de0c264217de
whlong1 <whunterlong@gmail.com>
2022-07-16 16:49:42 -0400

    build out guild index controller

--- controllers/guilds.js
+++ controllers/guilds.js
@@ -3,7 +3,17 @@ import { Profile } from "../models/profile/profile.js"
 
 const index = async (req, res) => {
   try {
-
+    const { search, page, sort } = req.query
+    const fields = 'name banner members public'
+    const limit = req.query.limit ? req.query.limit : 10
+    const filter = { name: { $regex: search, $options: 'i' } }
+    const order = { recent: { createdAt: 'desc' }, popular: { members: 'desc' } }
+    const guilds = await Guild.find(search ? filter : {}, fields)
+      .limit(limit)
+      .skip(parseInt(page) * limit)
+      .sort(sort ? order[sort] : order.recent)
+      .populate('administrators', 'name avatar', { options: { limit: 1 } })
+    res.status(200).json(guilds)
   } catch (err) {
     res.status(500).json(err)
   }
